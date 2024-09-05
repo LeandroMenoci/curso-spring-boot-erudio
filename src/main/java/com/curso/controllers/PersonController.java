d20390a48bc2aaf9452fdce45e0653a7d2d8ed43
Leandro <menoci.leandro@gmail.com>
2024-09-05 18:00:53 -0300

    Verbos HTTP

---
+++ src/main/java/com/curso/controllers/PersonController.java
@@ -0,0 +1,49 @@
+package com.curso.controllers;
+
+import com.curso.model.Person;
+import com.curso.services.PersonServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import com.curso.converters.NumberConverter;
+import com.curso.exceptions.UnsupportedMathOperationException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+    @Autowired
+    private PersonServices service;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findlAll() {
+        return service.findAll();
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person findById(@PathVariable(value = "id") String id) {
+        return service.findById(id);
+    }
+
+    @PostMapping (
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        return service.create(person);
+    }
+
+    @PutMapping (
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+    @DeleteMapping (value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@PathVariable(value = "id") String id) {
+        service.delete(id);
+    }
+}

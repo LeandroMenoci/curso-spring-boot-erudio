9d4dd75d2bb4639110f4b95c49e90aaa55f3725c
Leandro <menoci.leandro@gmail.com>
2024-09-12 10:45:49 -0300

    Adicionado VO (Value Obeject) e Dozer Mapping

--- src/main/java/com/curso/controllers/PersonController.java
+++ src/main/java/com/curso/controllers/PersonController.java
@@ -1,13 +1,10 @@
 package com.curso.controllers;
 
-import com.curso.model.Person;
+import com.curso.data.vo.v1.PersonVO;
 import com.curso.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-
-import com.curso.converters.NumberConverter;
-import com.curso.exceptions.UnsupportedMathOperationException;
 
 import java.util.List;
 
@@ -19,26 +16,26 @@ public class PersonController {
     private PersonServices service;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public List<Person> findlAll() {
+    public List<PersonVO> findlAll() {
         return service.findAll();
     }
 
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person findById(@PathVariable(value = "id") Long id) {
+    public PersonVO findById(@PathVariable(value = "id") Long id) {
         return service.findById(id);
     }
 
     @PostMapping (
             produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Person create(@RequestBody Person person) {
+    public PersonVO create(@RequestBody PersonVO person) {
         return service.create(person);
     }
 
     @PutMapping (
             produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Person update(@RequestBody Person person) {
+    public PersonVO update(@RequestBody PersonVO person) {
         return service.update(person);
     }
 
--- src/main/java/com/curso/services/PersonServices.java
+++ src/main/java/com/curso/services/PersonServices.java
@@ -6,8 +6,8 @@ import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.curso.data.vo.v1.PersonVO;
 import com.curso.exceptions.ResourceNotFoundException;
-import com.curso.model.Person;
 import com.curso.repositories.PersonRepository;
 
 @Service
@@ -18,14 +18,14 @@ public class PersonServices {
     @Autowired
     PersonRepository repository;
 
-    public List<Person> findAll(){
+    public List<PersonVO> findAll(){
 
         logger.info("Finding all people!");
 
         return repository.findAll();
     }
 
-    public Person findById(Long id) {
+    public PersonVO findById(Long id) {
 
         logger.info("Finding one person!");
 
@@ -34,16 +34,16 @@ public class PersonServices {
                         .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
     }
 
-    public Person create(Person person) {
+    public PersonVO create(PersonVO person) {
         logger.info("Updating one person!");
 
         return repository.save(person);
     }
 
-    public Person update(Person person) {
+    public PersonVO update(PersonVO person) {
         logger.info("Creating one person!");
         
-        Person entity = repository.findById(person.getId())
+        PersonVO entity = repository.findById(person.getId())
         .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 
         entity.setFirstName(person.getFirstName());
@@ -56,7 +56,7 @@ public class PersonServices {
     public void delete(Long id) {
         logger.info("Deleting one person!");
 
-        Person entity = repository.findById(id)
+        PersonVO entity = repository.findById(id)
         .orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 
         repository.delete(entity);
